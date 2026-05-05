ffd58c347ddb3098dac4dd4cd51f51fedc895cda
Compumarts <moazmohamed872@gamil.com>
2026-05-05 16:46:14 +0300

    added user

--- User.h
+++ User.h
@@ -1,10 +1,29 @@
-#ifndef REAL_STATE_SYSTEM_USER_H
-#define REAL_STATE_SYSTEM_USER_H
+#include <string>
+#include <iostream>
+#include <list>
 
 
-class User
-{
+class User {
+    std::string userName;
+    std::string name;
+    int phoneNumber;
+    int password;
+    std::list<int> listingIds;
+
+public:
+
+    User();
+    User( std::string user_name,  std::string ame, int phone_number, int password,
+    const std::list<int> &listing_ids);
+
+    std::string getUserName();
+    void setUserName(std::string );
+    std::string getName();
+    void setName(std::string);
+    int getPhoneNumber();
+    void setPhoneNumber(int);
+    int getPassword();
+    void setPassword(int password);
+    std::list<int> getListingIds();
+    void set_listing_ids(std::list<int>);
 };
-
-
-#endif //REAL_STATE_SYSTEM_USER_H
