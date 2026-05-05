c1a8213522aea9cf65734ff5c402c4cadc7a95ed
ossox878 <108822070+ossox878@users.noreply.github.com>
2026-05-05 10:50:20 +0300

    Refactor Listings class and add member functions
    listing.h

--- Listings.h
+++ Listings.h
@@ -6,8 +6,38 @@
 #define REAL_STATE_SYSTEM_LISTINGS_H
 
 
-class listings
-{
+#include<string>
+
+
+class Listing {
+    int id;
+    static int idCounter ;
+    std::string userName;
+    float price;
+    std::string location;
+    float size;
+    int numOfBedrooms;
+    bool Sold;
+
+public:
+    Listing();
+    Listing(int id,  std::string user_name, float price,  std::string location, float size,
+            int num_of_bedrooms, bool sold);
+
+    int getId();
+    std::string getUserName();
+    float getPrice();
+    std::string getLocation();
+    float getSize();
+    int getNumOfBedrooms();
+    bool isSold();
+    void setId(int id);
+    void setUserName(const std::string user_name);
+    void setPrice(float price);
+    void setLocation(const std::string location);
+    void setSize(float size);
+    void setNumOfBedrooms(int num_of_bedrooms);
+    void setIsSold(bool is_sold);
 };
 
 
