8c370cd5378cb8f760e15a84d5f55fb9b2612f58
Compumarts <moazmohamed872@gamil.com>
2026-05-05 16:48:28 +0300

    added listing

--- Listings.cpp
+++ Listings.cpp
@@ -1,5 +1,84 @@
-//
-// Created by Compumarts on 04-May-26.
-//
+#include "Listings.h"
 
-#include "listings.h"
+Listing::Listing() {
+    id = 0;
+    idCounter++;
+    userName = "";
+    price = 0.0;
+    location = "";
+    size = 0.0;
+    numOfBedrooms = 0;
+    Sold = false;
+
+}
+Listing::Listing(int id,  std::string user_name, float price, std::string location, float size,
+    int num_of_bedrooms, bool sold)
+        : id(id),
+          userName(user_name),
+          price(price),
+          location(location),
+          size(size),
+          numOfBedrooms(num_of_bedrooms),
+          Sold(sold) {
+    idCounter++;
+}
+
+int Listing :: getId() {
+    return id;
+}
+
+std :: string Listing :: getUserName()  {
+    return userName;
+}
+
+float Listing :: getPrice()  {
+    return price;
+}
+
+std::string Listing:: getLocation()  {
+    return location;
+}
+
+float Listing:: getSize() {
+    return size;
+}
+
+int Listing:: getNumOfBedrooms()  {
+    return numOfBedrooms;
+}
+
+bool Listing::isSold()  {
+    return Sold;
+}
+
+void Listing:: setId(int id) {
+    this->id = id;
+}
+
+void Listing:: setUserName(const std::string user_name) {
+    userName = user_name;
+}
+
+void Listing:: setPrice(float price) {
+    this->price = price;
+}
+
+void Listing:: setLocation(const std::string location) {
+    this->location = location;
+}
+
+void Listing::setSize(float size) {
+    this->size = size;
+}
+
+void Listing:: setNumOfBedrooms(int num_of_bedrooms) {
+    numOfBedrooms = num_of_bedrooms;
+}
+
+void Listing:: setIsSold(bool is_sold) {
+    Sold = is_sold;
+}
+int Listing:: getIdCounter() {
+    return idCounter;
+}
+```
--- Listings.h
+++ Listings.h
@@ -1,14 +1,33 @@
-//
-// Created by Compumarts on 04-May-26.
-//
+#include<string>
 
-#ifndef REAL_STATE_SYSTEM_LISTINGS_H
-#define REAL_STATE_SYSTEM_LISTINGS_H
+class Listing {
+    int id;
+    static int idCounter ;
+    std::string userName;
+    float price;
+    std::string location;
+    float size;
+    int numOfBedrooms;
+    bool Sold;
 
+public:
+    Listing();
+    Listing(int id,  std::string user_name, float price,  std::string location, float size,
+            int num_of_bedrooms, bool sold);
 
-class listings
-{
+    int getId();
+    std::string getUserName();
+    float getPrice();
+    std::string getLocation();
+    float getSize();
+    int getNumOfBedrooms();
+    bool isSold();
+    void setId(int id);
+    void setUserName(const std::string user_name);
+    void setPrice(float price);
+    void setLocation(const std::string location);
+    void setSize(float size);
+    void setNumOfBedrooms(int num_of_bedrooms);
+    void setIsSold(bool is_sold);
+    static int getIdCounter();
 };
-
-
-#endif //REAL_STATE_SYSTEM_LISTINGS_H
