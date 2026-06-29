5cbc014ab9e0a59439982c6748d43099e319002f
Khaled <khaledwaleed1542006elwaly@gmail.com>
2026-06-29 04:52:22 +0300

    Implement users serialization

--- fileSystem.cpp
+++ fileSystem.cpp
@@ -1,5 +1,6 @@
 #include "fileSystem.h"
-
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -7,4 +8,26 @@ void load_data(unordered_map<string, User> &user_data, unordered_map<int, Listin
 {
 }
 
-void save_data(unordered_map<string, User> &user_data, unordered_map<int, Listing> &listing_data){}
+void save_data(unordered_map<string, User> &user_data, unordered_map<int, Listing> &listing_data) {
+    ofstream usersFile("data/users.txt");
+    for (auto p : user_data)
+    {
+        User user = p.second;
+        usersFile
+                << user.getUserName() << '|'
+                << user.getName() << '|'
+                << user.getPhoneNumber() << '|'
+                << user.getPassword() << '|';
+        list<int> ids = user.getListingIds();
+        auto it = ids.begin();
+        while (it != ids.end())
+        {
+            usersFile << *it;
+            ++it;
+            if (it != ids.end())
+                usersFile << ',';
+        }
+        usersFile << '\n';
+    }
+    usersFile.close();
+}
